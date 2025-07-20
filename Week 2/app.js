25aa02cb3d2500acc18f051ff598307125df25bc
Konain Baig <konainbaig915@gmail.com>
2025-07-20 20:03:23 +0500

    practice

---
+++ Week 2/app.js
@@ -0,0 +1,65 @@
+// Grade checker using switch statement
+
+// let grade=prompt('Enter your grade:')
+// switch(grade){
+//     case'1':
+//     alert("you are in 1");
+//      break;
+//     case'2':
+//     alert("you are in 2");
+//      break;
+//     case'3':
+//     alert("you are in 3");
+//     break;
+//     default:
+//     console.log(`Sorry, we are out of `);
+// }
+
+// loop example (1-10) , even number
+// for loop
+
+// for(let i=2;i<=10; i+=2){
+// console.log(i);
+// }
+
+//while loop
+// let i=0
+// while(i<=10){
+//     console.log(i)
+
+//     i+=2
+// }
+
+
+//function,scope,return add 2 number
+
+// function add(a,b){
+    
+   
+//     return a+b
+// }
+//  let num1=parseInt(prompt("Enter first number"))
+//     let num2=parseInt(prompt("Enter second number"))
+// const result=(num1+num2)
+// console.log(`this is ${result}`)
+
+//greet user
+// function greetUser(name){
+//     return name
+// }
+
+// const userName=prompt("Enter your name");
+//ternary operator
+// const check= greetUser(userName)? `hello ${userName}`:'hello Unknown'
+// console.log(check);
+
+// if(userName){
+//     const greeting=greetUser(userName)
+//     console.log(`hello mr./ms. ${greeting}`)
+// }
+// else{
+//     console.log('Enter your name');
+    
+// }
+
+
